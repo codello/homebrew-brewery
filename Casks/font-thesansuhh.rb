c391988d874b8c9a4d93b2aa87bad417ad478474
Kim Wittenburg <5wittenb@informatik.uni-hamburg.de>
2018-03-05 21:44:53 +0100

    Add TheSansUHH Font

---
+++ Casks/font-thesansuhh.rb
@@ -0,0 +1,13 @@
+cask 'thesansuhh' do
+  version '1.003'
+  sha256 'b8c0467e09d516311094562b5d7bc41795677566423304c2c2cc50a8eaef99e7'
+
+  url "https://github.com/Codello/homebrew-brewery/raw/master/Resources/TheSansUHH/TheSansUHH_TTF_v1.003.zip"
+  name 'TheSansUHH'
+  homepage 'https://github.com/Codello/homebrew-brewery/tree/master/Resources/TheSansUHH'
+
+  font 'TheSansUHH Regular.ttf'
+  font 'TheSansUHH Regular Italic.ttf'
+  font 'TheSansUHH_TT_Bold_Caps.ttf'
+  font 'TheSansUHH_TT_SemiLight_Caps.ttf'
+end
