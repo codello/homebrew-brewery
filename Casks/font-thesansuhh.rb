8a33e724cb105a6a1e3045e794c019e05823ebcc
Kim Wittenburg <5wittenb@informatik.uni-hamburg.de>
2018-03-06 12:54:13 +0100

    Move to GitLab

--- Casks/font-thesansuhh.rb
+++ Casks/font-thesansuhh.rb
@@ -2,9 +2,9 @@ cask 'font-thesansuhh' do
   version '1.003'
   sha256 'b8c0467e09d516311094562b5d7bc41795677566423304c2c2cc50a8eaef99e7'
 
-  url "https://github.com/Codello/homebrew-brewery/raw/master/Resources/TheSansUHH/TheSansUHH_TTF_v1.003.zip"
+  url "https://gitlab.com/Codello/homebrew-brewery/raw/master/Resources/TheSansUHH/TheSansUHH_TTF_v1.003.zip"
   name 'TheSansUHH'
-  homepage 'https://github.com/Codello/homebrew-brewery/tree/master/Resources/TheSansUHH'
+  homepage 'https://gitlab.com/Codello/homebrew-brewery/tree/master/Resources/TheSansUHH'
 
   font 'TheSansUHH Regular.ttf'
   font 'TheSansUHH Regular Italic.ttf'
