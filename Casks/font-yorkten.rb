2b221c48838b4e64bb9a915733f28393b414343b
Kim Wittenburg <5wittenb@informatik.uni-hamburg.de>
2018-03-06 12:54:36 +0100

    Add Yorkten

---
+++ Casks/font-yorkten.rb
@@ -0,0 +1,10 @@
+cask 'font-yorkten' do
+  version '1.0'
+  sha256 '81f982760c588afb871ff2c340eac4f812aed056aa93a38882efb5fe0aad83a5'
+
+  url "https://gitlab.com/Codello/homebrew-brewery/raw/master/Resources/Yorkten/yorkten.zip"
+  name 'Yorkten'
+  homepage 'https://github.com/Codello/homebrew-brewery/tree/master/Resources/Yorkten'
+
+  font '*.ttf'
+end
