9cb100aa701c876c7abe2eb2c5d33ee27dfc76f1
Kim Wittenburg <5wittenb@informatik.uni-hamburg.de>
2018-03-12 23:08:11 +0100

    Add Login Possibility

--- Casks/font-yorkten.rb
+++ Casks/font-yorkten.rb
@@ -2,7 +2,7 @@ cask 'font-yorkten' do
   version '1.0'
   sha256 '81f982760c588afb871ff2c340eac4f812aed056aa93a38882efb5fe0aad83a5'
 
-  url "https://gitlab.com/Codello/homebrew-brewery/raw/master/Resources/Yorkten/yorkten.zip"
+url "https://gitlab.com/Codello/homebrew-brewery/raw/master/Resources/Yorkten/yorkten.zip?private_token=#{ENV['GITLAB_HOMEBREW_TOKEN']}"
   name 'Yorkten'
   homepage 'https://gitlab.com/Codello/homebrew-brewery/tree/master/Resources/Yorkten'
 
