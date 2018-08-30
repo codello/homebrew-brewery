82299f95ce0d7c8c9c32f67063edb32a2d40105b
Kim Wittenburg <5wittenb@informatik.uni-hamburg.de>
2018-08-30 11:55:34 +0200

    Add Font Corbel

---
+++ Casks/font-corbel.rb
@@ -0,0 +1,16 @@
+cask 'font-corbel' do
+  version '1.0'
+  sha256 '81f982760c588afb871ff2c340eac4f812aed056aa93a38882efb5fe0aad83a5'
+
+  url "https://www.wfonts.com/download/data/2014/11/28/corbel/corbel.zip"
+  name 'Corbel'
+  homepage 'https://www.myfonts.com/fonts/ascender/corbel/'
+
+  font 'Corbel Bold Italic.ttf'
+  font 'Corbel Bold.ttf'
+  font 'Corbel Italic.ttf'
+  font 'CORBEL.TTF'
+  font 'CORBELB.TTF'
+  font 'corbeli.ttf'
+  font 'corbelz.ttf'
+end
