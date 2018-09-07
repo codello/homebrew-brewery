e032fd16f51d7bab08dda152683d6b7e8190ba82
Kim Wittenburg <5wittenb@informatik.uni-hamburg.de>
2018-09-07 17:43:18 +0200

    Add font-hk-grotesk

---
+++ Casks/font-hk-grotesk.rb
@@ -0,0 +1,29 @@
+cask 'font-hk-grotesk' do
+  version '1.0'
+  sha256 '3eb644374e7df186dde06431f3ae957bad4b8790f619dc21f565593253ca1ae7'
+
+  url "https://www.fontsquirrel.com/fonts/download/hk-grotesk"
+  name 'HK Grotesk'
+  homepage 'https://hanken.co/product/hk-grotesk/'
+
+  font 'HKGrotesk-Bold.otf'
+  font 'HKGrotesk-BoldItalic.otf'
+  font 'HKGrotesk-BoldLegacy.otf'
+  font 'HKGrotesk-BoldLegacyItalic.otf'
+  font 'HKGrotesk-Italic.otf'
+  font 'HKGrotesk-LegacyItalic.otf'
+  font 'HKGrotesk-Light.otf'
+  font 'HKGrotesk-LightItalic.otf'
+  font 'HKGrotesk-LightLegacy.otf'
+  font 'HKGrotesk-LightLegacyItalic.otf'
+  font 'HKGrotesk-Medium.otf'
+  font 'HKGrotesk-MediumItalic.otf'
+  font 'HKGrotesk-MediumLegacy.otf'
+  font 'HKGrotesk-MediumLegacyItalic.otf'
+  font 'HKGrotesk-Regular.otf'
+  font 'HKGrotesk-RegularLegacy.otf'
+  font 'HKGrotesk-SemiBold.otf'
+  font 'HKGrotesk-SemiBoldItalic.otf'
+  font 'HKGrotesk-SemiBoldLegacy.otf'
+  font 'HKGrotesk-SemiBoldLegacyItalic.otf'
+end
