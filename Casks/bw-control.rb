4b70e951a1911fbae99b07fe661e71372e588cff
Kim Wittenburg <codello@wittenburg.kim>
2019-10-20 18:53:16 +0200

    Add Bowers & Wilkins Control App

---
+++ Casks/bw-control.rb
@@ -0,0 +1,10 @@
+cask 'bw-control' do
+  version '1.2.3'
+  sha256 'ad6f2c1ad1cf6988f41d183d73894d9fcb4d92985807b9891f14d7a104ecf4d3'
+
+  url "https://bwfirmware.com/software/airplay_setup/mac/180327_Bowers&WilkinsControl_1.2.3_14667.dmg"
+  name 'Bowers & Wilkins Control'
+  homepage 'https://www.bowerswilkins.com/de-de/wireless-speakers-control-app'
+
+  app 'Bowers & Wilkins Control.app'
+end
