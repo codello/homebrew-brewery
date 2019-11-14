07ac23a997ae9ae9f77739a11862bb59d94e7f31
Kim Wittenburg <codello@wittenburg.kim>
2019-11-14 14:38:19 +0100

    Update SRTLab

--- Formula/srtlab.rb
+++ Formula/srtlab.rb
@@ -1,8 +1,9 @@
 class Srtlab < Formula
+    version "0.98"
     desc "SubRip subtitle file converter"
     homepage "https://www.dr-lex.be/software/srtlab.html"
-    url "https://github.com/DrLex0/SRTLab/releases/download/0.97/srtlab.pl"
-    sha256 "6d3f49c310a9b08c4a7c8fdb59c898286a3b70b481f78ef4e947af6948eee08a"
+    url "https://github.com/DrLex0/SRTLab/releases/download/0.98/srtlab.pl"
+    sha256 "356ef5a4a826e23b2e554789c96ee8452093ed0de13f049656f2b5c8039f51d9"
 
     def install
         mv "srtlab.pl", "srtlab"
