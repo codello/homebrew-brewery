af20c393a2b67602217f38557c1e82ab7f398371
Kim Wittenburg <codello@wittenburg.kim>
2020-04-22 20:59:30 +0200

    Update M4VGear URL

--- Casks/m4vgear.rb
+++ Casks/m4vgear.rb
@@ -2,7 +2,7 @@ cask 'm4vgear' do
   version '4.3.8'
   sha256 'dc17bed490803780cf8c30ab01cb13dca56a886e61d2da3391886e3e129797b0'
 
-  url 'http://www.m4vgear.com/m4vgear.dmg'
+  url 'https://www.m4vgear.com/m4vgear.dmg'
   name 'M4VGear'
   homepage 'http://www.m4vgear.de'
 
