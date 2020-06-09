f7d17ad9cf06b31420b7d9d2642b53eb7a1b17dc
Kim Wittenburg <codello@wittenburg.kim>
2020-06-09 03:16:32 +0200

    Remove explicit version

--- Formula/extractpdfmark.rb
+++ Formula/extractpdfmark.rb
@@ -1,7 +1,6 @@
 class Extractpdfmark < Formula
   desc "Extract page mode and named destinations as PDFmark from PDF"
-  homepage "https://github.com/trueroad/extractpdfmark"
-  version "1.1.0"
+  homepage "https://github.com/trueroad/
   url "https://github.com/trueroad/extractpdfmark/releases/download/v1.1.0/extractpdfmark-1.1.0.tar.gz"
   sha256 "0935045084211fcf68a9faaba2b65c037d0adfd7fa27224d2b6c7ae0fd7964cb"
 
