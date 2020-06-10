211f6619535d60f13f515968d31b27b9d36e9374
Kim Wittenburg <codello@wittenburg.kim>
2020-06-10 12:34:14 +0200

    Move UltraStar to casks

---
+++ Casks/ultrastar-deluxe.rb
@@ -0,0 +1,11 @@
+cask "ultrastar-deluxe" do
+  version "2020.4.0"
+  sha256 "21c46ed7cc77fd4678b395fc9cc346c572cc5798df8479392968cb684ca2b5d9"
+
+  # github.com/UltraStar-Deluxe was verified as official when first introduced to the cask
+  url "https://github.com/UltraStar-Deluxe/USDX/releases/download/v2020.4.0/UltraStar.Deluxe-v2020.4.0.stable_macOS.dmg"
+  name "UltraStar Deluxe"
+  homepage "https://usdx.eu/"
+
+  app "UltraStarDeluxe.app"
+end
