c19eca5ca14d6e09d572ae72d674c99730277159
codello <kim@codello.dev>
2024-03-26 16:15:53 +0100

    Remove appcast

--- Casks/ultrastar-deluxe.rb
+++ Casks/ultrastar-deluxe.rb
@@ -1,10 +1,9 @@
 cask "ultrastar-deluxe" do
-  version "2023.6.0"
-  sha256 "a0d47271ae836fd2fccf6962ca127e1ad0313a6ea3f455ad336510e13a85ded0"
+  version "2024.3.0"
+  sha256 "73a2ca351bbdf74192d961fc517be9e2f2b1c3e0ba8f84d45bd980e5253ba0ff"
 
-  url "https://github.com/UltraStar-Deluxe/USDX/releases/download/v2023.6.0/UltraStarDeluxe-v2023.6.0-0.dmg",
+  url "https://github.com/UltraStar-Deluxe/USDX/releases/download/v2024.3.0/UltraStarDeluxe-v2024.3.0.dmg",
       verified: "github.com/UltraStar-Deluxe/USDX/"
-  appcast "https://github.com/UltraStar-Deluxe/USDX/releases.atom"
   name "UltraStar Deluxe"
   desc "UltraStar Deluxe is an open source karaoke party game"
   homepage "https://usdx.eu/"
--- Casks/ultrastar-manager.rb
+++ Casks/ultrastar-manager.rb
@@ -3,7 +3,6 @@ cask "ultrastar-manager" do
   sha256 "799d1b32dcaad0adf47488e9a601dab4049b126936a2533c090d0683bb848eb4"
 
   url "https://github.com/UltraStar-Deluxe/UltraStar-Manager/releases/download/v1.8.3/uman-1.8.3.dmg"
-  appcast "https://github.com/UltraStar-Deluxe/UltraStar-Manager/releases.atom"
   name "UltraStar Manager"
   desc "Manager application for UltraStar songs"
   homepage "https://github.com/UltraStar-Deluxe/UltraStar-Manager/"
