a4194fb83da740ff3d6dd7fda6f5b732211db1c3
codello <kim@codello.dev>
2024-03-28 16:13:11 +0100

    Use version modifiers

--- Casks/ultrastar-creator.rb
+++ Casks/ultrastar-creator.rb
@@ -3,7 +3,7 @@ cask "ultrastar-creator" do
   version "1.2.0"
   sha256 "7ca4342588b9c32f3f0ee33a964f1562fbe5619dcec3464c7a1e42fcd9ed83c5"
 
-  url "https://downloads.sourceforge.net/usc/UltraStarCreator-#{version.sub(/\.\d+$/, "")}.dmg",
+  url "https://downloads.sourceforge.net/usc/UltraStarCreator-#{version.major_minor}.dmg",
       verified: "downloads.sourceforge.net/usc/"
   name "UltraStar Creator"
   desc "Create UltraStar karaoke songs from scratch"
