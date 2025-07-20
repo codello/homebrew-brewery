d53f5b8d73e70b6f80b60aeb99efa2d99826471d
Kim Wittenburg <kim@codello.dev>
2025-07-20 20:41:14 +0200

    Fix code style

--- Casks/ultrastar-creator.rb
+++ Casks/ultrastar-creator.rb
@@ -3,7 +3,7 @@ cask "ultrastar-creator" do
   arch arm: "ARM", intel: "x86"
 
   version "1.3.1"
-  sha256 arm: "312fffe016282ef68a06b64e9049133b9e3b622d848f7acd6111fe9d39ef9132",
+  sha256 arm:   "312fffe016282ef68a06b64e9049133b9e3b622d848f7acd6111fe9d39ef9132",
          intel: "88b909eb272713bf778c3c376ce8fd39489b99c120a4235468f9f423d223bfa3"
 
   url "https://github.com/UltraStar-Deluxe/UltraStar-Creator/releases/download/#{version}/MAC-#{arch}-UltraStar-Creator.dmg"
--- Casks/ultrastar-manager.rb
+++ Casks/ultrastar-manager.rb
@@ -2,7 +2,7 @@ cask "ultrastar-manager" do
   arch arm: "ARM", intel: "x86"
 
   version "2.0.0"
-  sha256 arm: "23b38629e5291ef17514f824aa8080441ead829769fd2b9a45ab544bf4210290",
+  sha256 arm:   "23b38629e5291ef17514f824aa8080441ead829769fd2b9a45ab544bf4210290",
          intel: "2818e9c0258360460ec1a6825770dc5a91962a30b081b64c6fe681153722fec6"
 
   url "https://github.com/UltraStar-Deluxe/UltraStar-Manager/releases/download/#{version}/MAC-#{arch}-UltraStar-Manager.dmg"
