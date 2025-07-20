b4bb49f172fac9600b32af6a42031da93b371a68
Kim Wittenburg <kim@codello.dev>
2025-07-20 20:36:12 +0200

    Fix version

--- Casks/ultrastar-manager.rb
+++ Casks/ultrastar-manager.rb
@@ -1,7 +1,7 @@
 cask "ultrastar-manager" do
   arch arm: "ARM", intel: "x86"
 
-  version "2.0.ß"
+  version "2.0.0"
   sha256 arm: "23b38629e5291ef17514f824aa8080441ead829769fd2b9a45ab544bf4210290",
          intel: "2818e9c0258360460ec1a6825770dc5a91962a30b081b64c6fe681153722fec6"
 
